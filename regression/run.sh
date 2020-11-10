89f062e8ea46ce5ecee7bd0140d7947c09d04b1d
padoura <pantourakis.michail@gmail.com>
2020-11-10 17:58:16 +0200

    Improved run.sh (-c -t as optional arguments, greping FAILED tests)

--- regression/run.sh
+++ regression/run.sh
@@ -8,31 +8,60 @@ else
     suite=$1
 fi
 
-echo "Testing suite $suite..."
-
-./validate_api -c 1,2,4 -t 0 $suite > regression/results/results_$suite.txt 2>&1
-
-if [ ! -f regression/results/golden_$suite.txt ]; then
-    echo "" > regression/results/golden_$suite.txt
+if [[ "$2" == "-c" && !(-z $3) ]]; then
+    coreNum=$3
+    if [[ "$4" == "-t" && !(-z "$5") ]]; then
+        termNum=$5
+    else
+        termNum=0
+    fi
+else
+    coreNum=1
+    termNum=0
 fi
 
-diffResult=$(diff regression/results/golden_$suite.txt regression/results/results_$suite.txt)
+
+echo "Number of cores: $coreNum"
+echo "Number of terminals: $termNum"
+echo "Testing suite $suite..."
+
+fileId=${suite}_${coreNum}_${termNum}
+./validate_api -c $coreNum -t $termNum $suite > regression/results/results_$fileId.txt 2>&1
+
+if [ ! -f regression/results/golden_$fileId.txt ]; then
+    echo "" > regression/results/golden_$fileId.txt
+fi
+
+echo -e "\n********************\n"
+
+grepResult=$(grep "FAILED" regression/results/golden_$fileId.txt)
+if [[ -z $grepResult ]]; then
+    echo "no FAILED tests!"
+else
+    echo -e "FAILED tests found:\n"
+    grep "FAILED" regression/results/golden_$fileId.txt | cut -d" " -f9
+fi
+
+echo -e "********************\n"
+
+diffResult=$(diff regression/results/golden_$fileId.txt regression/results/results_$fileId.txt)
 
 if [[ -z $diffResult ]]; then
     echo "no diff"
 else
     echo -e "diff found:\n--------------------"
-    colordiff regression/results/golden_$suite.txt regression/results/results_$suite.txt
+    colordiff regression/results/golden_$fileId.txt regression/results/results_$fileId.txt
     echo "--------------------"
     echo "Replace golden? [y/(n)]"
     read isGolden
     isGolden=${isGolden:-n}
 
     if [ "$isGolden" == "y" ]; then
-        mv regression/results/results_$suite.txt regression/results/golden_$suite.txt
+        mv regression/results/results_$fileId.txt regression/results/golden_$fileId.txt
         echo "Golden replaced"
     else
         echo "Golden not replaced"
     fi
 fi
+
 
